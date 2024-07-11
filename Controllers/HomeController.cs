1f1b3ceea7b53bbc486be6630d9891dbfa8b13bd
Viral Rajput <Viral Rajput@LAPTOP-KU7I3IVU>
2024-07-11 10:55:44 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcCRUDoperation.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCEntities context = new MVCEntities();
+
+        public ActionResult Index()
+        {
+            var listofData = context.Employees.ToList();
+
+            return View(listofData);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Create( Employee model) 
+        {
+            context.Employees.Add(model);
+            context.SaveChanges();
+            ViewBag.Message = "Data Add Successfully";   
+            return View();
+
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+        var data = context.Employees.Where(x=> x.EmpID == ID).FirstOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee model) 
+        {
+            var data = context.Employees.Where(x => x.EmpID == model.EmpID).FirstOrDefault();
+        if(data != null)
+            {
+             data.EmpName = model.EmpName;   
+             data.Email = model.Email;
+             data.Salary = model.Salary;   
+             data.City = model.City;
+
+                context.SaveChanges();
+
+            }
+        return RedirectToAction("Index");
+        }
+    
+        public ActionResult Detail() 
+        {
+            var data = context.Employees.Where(x => x.EmpID == x.EmpID).FirstOrDefault();
+            return View(data);
+        
+        }
+
+        public ActionResult Delete(int id) 
+        {
+            var data = context.Employees.Where(x => x.EmpID == x.EmpID).FirstOrDefault();
+            context.Employees.Remove(data);
+            context.SaveChanges();
+            ViewBag.Message = "Delete successfully";
+            return RedirectToAction("index");
+
+        }
+
+
+
+    }
+
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcCRUDoperation.MvcApplication" Language="C#" %>
